6a7c31c679a483772a7f3fc8e223eb64b36db016
Pooya <ibtkm2009@gmail.com>
2014-04-04 00:24:51 +0430

    here we go!

---
+++ agent.rb
@@ -0,0 +1,28 @@
+require 'nokogiri'
+require 'open-uri'
+require 'colorize'
+
+blacklist = ["dvdrip","xvid","maxspeed","720p","hd"]
+
+puts "Enter the directory: "
+dir_addr = gets.chomp
+names = Dir.entries(dir_addr).reject { |name| name == "." or name == ".." }
+names.each do |name|
+    name.downcase!
+    blacklist.each { |black_name| name.gsub!(black_name,'') if name.include? black_name }
+    name = name.gsub(/[^A-Za-z\s]/,' ').strip.gsub(" ","+")
+    doc = Nokogiri::HTML(open("http://www.imdb.com/find?q=#{name}&s=all"))
+    section = doc.css("div.findSection").select { |section| section.css("h3.findSectionHeader").text == "Titles" }.first
+    relative_url = section.css("table.findList").css("tr").first.css("td.result_text").css("a").attr("href").text
+    doc = Nokogiri::HTML(open("http://www.imdb.com#{relative_url}"))
+    puts ">>> #{doc.css("h1.header").css("span.itemprop").text} <<<".red
+    print "genre: ".green
+    print doc.css("div.infobar").css("span.itemprop").map { |item| item.text }
+    print "\n"
+    print "rating: ".green
+    puts doc.css("div.star-box-giga-star").text
+    puts "description: ".green
+    puts doc.css("div.star-box-giga-star").text
+    puts doc.css("td#overview-top").css("p").select { |p| p.attributes["itemprop"] and p.attributes["itemprop"].value == "description" }.first.text
+end
+
